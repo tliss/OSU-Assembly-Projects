fe017be1d7c45d51fb96bcd633d6932b854e2d5e
Taylor A. Liss <tayloraliss@gmail.com>
2017-04-15 14:47:07 -0400

    Update header info

---
+++ Prog01.asm
@@ -0,0 +1,33 @@
+
+TITLE Prog01 - Elementary Arithmetic     (Prog01.asm)
+
+; Author: Taylor Liss
+; Email: lisst@oregonstate.edu
+; Course / Project ID  CS271_400_S2017 / Prog01
+; Date: 4/15/2017
+; Description: This program dsplays my name and the program
+;	title on the output screen and instructs the user to
+;	enter in two numbers. It then calculates the sum,
+;	difference, product, (integer) quotent, and remainder
+;	of the two numbers. It then displays a terminating
+;	message.
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main

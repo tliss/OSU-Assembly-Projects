23cdf26c422ab4cf5a162b2657b958348c0a3a22
Taylor Liss <tayloraliss@gmail.com>
2017-04-23 12:45:33 -0400

    Columns almost working

--- Project02/Prog02.asm
+++ Project02/Prog02.asm
@@ -19,6 +19,7 @@ INCLUDE Irvine32.inc
 .data
 rangeNum	DWORD	?		;range of fibonacci terms
 nextNum		DWORD	?		;next fibonacci number in the sequence
+columnNum	DWORD	?		;counter for column place
 
 UPPER_LIMIT = 46	;max size for Fibonacci range
 LOWER_LIMIT = 1		;min size for Fibonacci range
@@ -33,6 +34,17 @@ fibPrompt2	BYTE	"Give the number as an integer in the range [1 .. 46]", 0dh, 0ah
 fibPrompt3	BYTE	"How many Fibonacci terms do you want? ", 0
 certMes		BYTE	"Results certified by Taylor Liss", 0dh, 0ah, 0
 goodbyeMes	BYTE	"Goodbye, ", 0
+
+space1		BYTE	" ", 0
+space2		BYTE	"  ", 0
+space3		BYTE	"   ", 0
+space4		BYTE	"    ", 0
+space5		BYTE	"     ", 0
+space6		BYTE	"      ", 0
+space7		BYTE	"       ", 0
+space8		BYTE	"        ", 0
+space9		BYTE	"         ", 0
+space10		BYTE	"          ", 0
 
 invalidMes	BYTE	"Out of range. Enter a number in [1 .. 46]", 0
 
@@ -86,16 +98,49 @@ rangePrompt:	;Get range
 	mov		ebx, 1
 	mov		ecx, rangeNum
 	mov		nextNum, 1
+	mov		columnNum, 1
 fibLoop:	;displayFibs
 	mov		nextNum, eax
 	add		eax, ebx
 	call	WriteDec
-	call	CrLf
 	mov		ebx, nextNum
+
+	cmp		eax, 10
+	jl		addTenSpace
+
+	cmp		eax, 100
+	jl		addNineSpace
+
+	cmp		eax, 1000
+	jl		addEightSpace
+
+	cmp		eax, 10000
+	jl		addSevenSpace
+
+	cmp		eax, 100000
+	jl		addSixSpace
+
+	cmp		eax, 1000000
+	jl		addFiveSpace
+
+	cmp		eax, 10000000
+	jl		addFourSpace
+
+	cmp		eax, 100000000
+	jl		addThreeSpace
+
+	cmp		eax, 1000000000
+	jl		addTwoSpace
+
+	cmp		eax, 1000000000
+	jl		addOneSpace
+
+continueLoop:
 	loop	fibLoop
 
 ;-----farewell-----
 ;Farewell
+	call	CrLf
 	call	CrLf
 	mov		edx, OFFSET certMes
 	call	WriteString
@@ -112,6 +157,67 @@ notValid:	;invalid range entered
 	call	WriteString
 	call	CrLf
 	jmp		rangePrompt
+
+addTenSpace:
+	mov		edx, OFFSET space10
+	call	WriteString
+	jmp		columnCheck
+
+addNineSpace:
+	mov		edx, OFFSET space9
+	call	WriteString
+	jmp		columnCheck
+
+addEightSpace:
+	mov		edx, OFFSET space8
+	call	WriteString
+	jmp		columnCheck
+
+addSevenSpace:
+	mov		edx, OFFSET space7
+	call	WriteString
+	jmp		columnCheck
+
+addSixSpace:
+	mov		edx, OFFSET space6
+	call	WriteString
+	jmp		columnCheck
+
+addFiveSpace:
+	mov		edx, OFFSET space5
+	call	WriteString
+	jmp		columnCheck
+
+addFourSpace:
+	mov		edx, OFFSET space4
+	call	WriteString
+	jmp		columnCheck
+
+addThreeSpace:
+	mov		edx, OFFSET space3
+	call	WriteString
+	jmp		columnCheck
+
+addTwoSpace:
+	mov		edx, OFFSET space2
+	call	WriteString
+	jmp		columnCheck
+
+addOneSpace:
+	mov		edx, OFFSET space1
+	call	WriteString
+	jmp		columnCheck
+
+columnCheck:
+	cmp		columnNum, 5
+	je		addRow
+	inc		columnNum
+	jmp		continueLoop
+
+addRow:		;if a number is the fifth number in a row
+	call	CrLf
+	mov		columnNum, 1
+	jmp		continueLoop
 
 main ENDP
 

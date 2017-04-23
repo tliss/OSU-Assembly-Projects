167378a729bef862e5a87c23cde64649b952d26a
Taylor A. Liss <tayloraliss@gmail.com>
2017-04-23 14:24:03 -0400

    Add EC1 info

--- Project02/Prog02.asm
+++ Project02/Prog02.asm
@@ -5,12 +5,11 @@ TITLE Prog02 - Fibonacci Numbers     (Prog02.asm)
 ; Email: lisst@oregonstate.edu
 ; Course / Project ID  CS271_400_S2017 / Prog02
 ; Date: 4/19/2017
-; Description: This program dsplays my name and the program
-;	title on the output screen and instructs the user to
-;	enter in two numbers. It then calculates the sum,
-;	difference, product, (integer) quotent, and remainder
-;	of the two numbers. It then displays a terminating
-;	message.
+; Description: This program calculates Fibonacci numbers. It
+;	asks for the users name and displays a greeting and a
+;	farewell to them. It also validates that the user only
+;	enters terms between the range [1 .. 46]. It displays
+;	the results in aligned columns.
 
 INCLUDE Irvine32.inc
 
@@ -27,6 +26,7 @@ LOWER_LIMIT = 1		;min size for Fibonacci range
 userName	DWORD	80 DUP (?)
 
 titleMes	BYTE	"		Prog02 - Fibonacci Numbers		by Taylor Liss", 0dh, 0ah, 0
+EC1Mes		BYTE	"**EC #1: This program list the Fibonacci terms in aligned columns", 0dh, 0ah, 0
 nameMes		BYTE	"What's your name? ", 0
 helloMes	BYTE	"Hello, ", 0
 fibPrompt	BYTE	"Enter the number of Fibonacci terms to be displayed", 0dh, 0ah, 0
@@ -35,7 +35,9 @@ fibPrompt3	BYTE	"How many Fibonacci terms do you want? ", 0
 certMes		BYTE	"Results certified by Taylor Liss", 0dh, 0ah, 0
 goodbyeMes	BYTE	"Goodbye, ", 0
 
-space1		BYTE	" ", 0
+invalidMes	BYTE	"Out of range. Enter a number in [1 .. 46]", 0
+
+;**EC 1: The following strings are used to display the numbers in aligned columns
 space2		BYTE	"  ", 0
 space3		BYTE	"   ", 0
 space4		BYTE	"    ", 0
@@ -46,14 +48,14 @@ space8		BYTE	"        ", 0
 space9		BYTE	"         ", 0
 space10		BYTE	"          ", 0
 
-invalidMes	BYTE	"Out of range. Enter a number in [1 .. 46]", 0
-
 .code
 main PROC
 ;-----introduction-----
 ;Display program title
 	mov		edx, OFFSET titleMes
 	call	WriteString	
+	mov		edx, OFFSET EC1Mes
+	call	WriteString
 	call	CrLf
 
 ;Get user name
@@ -105,6 +107,7 @@ fibLoop:	;displayFibs
 	call	WriteDec
 	mov		ebx, nextNum
 
+;EC1: This code formats the strings into columns based on the length of the length of the integer
 	cmp		eax, 10
 	jl		addTenSpace
 
@@ -132,8 +135,7 @@ fibLoop:	;displayFibs
 	cmp		eax, 1000000000
 	jl		addTwoSpace
 
-	cmp		eax, 1000000000
-	jl		addOneSpace
+	jmp		columnCheck	;This ensures that a column check will always be done
 
 continueLoop:
 	loop	fibLoop
@@ -158,6 +160,7 @@ notValid:	;invalid range entered
 	call	CrLf
 	jmp		rangePrompt
 
+;EC 1: The following labels are used to format the numbers into columns
 addTenSpace:
 	mov		edx, OFFSET space10
 	call	WriteString
@@ -203,18 +206,14 @@ addTwoSpace:
 	call	WriteString
 	jmp		columnCheck
 
-addOneSpace:
-	mov		edx, OFFSET space1
-	call	WriteString
-	jmp		columnCheck
-
+;This checks to see if the last number was placed into the fifth column
 columnCheck:
 	cmp		columnNum, 5
 	je		addRow
 	inc		columnNum
 	jmp		continueLoop
 
-addRow:		;if a number is the fifth number in a row
+addRow:		;adds a new row if the last number printed was the fifth number in a row.
 	call	CrLf
 	mov		columnNum, 1
 	jmp		continueLoop

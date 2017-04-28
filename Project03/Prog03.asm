1827d611681c634624e54c52c188005be2e690b1
Taylor Liss <tayloraliss@gmail.com>
2017-04-28 11:54:58 -0400

    Add coe to get user entry and some looping

--- Project03/Prog03.asm
+++ Project03/Prog03.asm
@@ -22,9 +22,19 @@ projName		BYTE	"Prog03 - Integer Accumulator", 0dh, 0ah, 0
 authName		BYTE	"By Taylor Liss", 0dh, 0ah, 0
 reqNameMess		BYTE	"What is your name? ", 0
 helloMes		BYTE	"Hello, ", 0
+instructMess1	BYTE	"Please enter numbers in [-100, -1].", 0dh, 0ah, 0
+instructMess2	BYTE	"Enter a non-negative number when you are finished to see results."
+numberPrompt	BYTE	"Number #", 0
+colon			BYTE	": ", 0
+tooSmallMess	BYTE	"The number you entered is smaller than -100.", 0dh, 0ah, 0
 
 userName		DWORD	80 DUP (?)
 
+LOWER_LIMIT = -100
+UPPER_LIMIT = -1
+
+totalNums		DWORD	?
+userNum			DWORD	?
 
 
 .code
@@ -53,6 +63,41 @@ startPoint: ;Program restarts to here when request.
 	mov		edx, OFFSET userName
 	call	WriteString
 	call	CrLf
+
+;Display isntructions
+	mov		edx, OFFSET instructMess1
+	call	WriteString
+	mov		edx, OFFSET instructMess2
+	call	WriteString
+
+	mov		totalNums, 0	;setup totalNums
+
+L1:
+	inc		totalNums
+	mov		edx, OFFSET numberPrompt
+	call	WriteString
+	mov		eax, totalNums
+	call	WriteDec
+	mov		edx, OFFSET	colon
+	call	WriteString
+
+	call	ReadInt
+	mov		userNum, eax
+	cmp		userNum, LOWER_LIMIT
+	jl		tooSmall
+	cmp		userNum, UPPER_LIMIT
+	jmp		finished
+
+tooSmall:
+	mov		edx, OFFSET tooSmall
+	call	WriteString
+	mov		edx, OFFSET	instructMess1
+	call	WriteString
+	dec		totalNums
+	jmp		L1
+
+finished:
+
 
 	exit	; exit to operating system
 main ENDP

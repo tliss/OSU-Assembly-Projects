9ebc650cf94a00c28d6f3c97c26a1158e5cfbb53
Taylor Liss <tayloraliss@gmail.com>
2017-05-28 15:11:35 -0400

    Program complete. Documentation added

--- Project05/Prog05.asm
+++ Project05/Prog05.asm
@@ -15,10 +15,10 @@ INCLUDE Irvine32.inc
 
 .data
 
-MAX = 200
-MIN = 10
-HI = 999
-LO = 100
+MAX = 200	;max number of integers to display
+MIN = 10	;minimum number of integers to display
+HI = 999	;highest number to be randomly generated
+LO = 100	;lowest number to be randomly generated
 
 userNum		DWORD	?				;number as entered by the user
 numArray	DWORD	MAX		DUP(?)	;array of random numbers
@@ -33,12 +33,14 @@ errorMess	BYTE	"Invalid input", 0dh, 0ah, 0
 display01	BYTE	"The unsorted random numbers:", 0dh, 0ah, 0
 spaces		BYTE	"   ", 0
 display02	BYTE	"The sorted list:", 0dh, 0ah, 0
+display03	BYTE	"The median is ", 0
 
 .code
 main PROC
 
 	call Randomize
 
+	;Introduction
 	push OFFSET intro01
 	push OFFSET intro02
 	push OFFSET intro03
@@ -47,18 +49,21 @@ main PROC
 
 	call CrLf
 
+	;Get user data
 	push userNum
 	push OFFSET prompt01
 	push OFFSET errorMess
 	call getData
 	mov	 userNum, eax
 
+	;Fill array with random numbers
 	push OFFSET numArray
 	push userNum
 	call fillArray
 
 	call CrLf
 
+	;Display array unsorted array
 	mov	 edx, OFFSET display01
 	call WriteString
 	push OFFSET numArray
@@ -69,10 +74,22 @@ main PROC
 	call CrLf
 	call CrLf
 
+	;Sort array	
 	push OFFSET numArray
 	push userNum
 	call sortList
 
+	;Display median number
+	mov	 edx, OFFSET display03
+	call WriteString
+	push OFFSET numArray
+	push userNum
+	call displayMedian
+
+	call CrLf
+	call CrLf
+
+	;Display sorted array
 	mov	 edx, OFFSET display02
 	call WriteString
 	push OFFSET numArray
@@ -81,29 +98,31 @@ main PROC
 	call displayArray
 
 	call CrLf
+	call CrLf
 
 	exit
 
 main ENDP
 
 ;----------------------------------------
-;
-;Receives:
-;Returns:
-;Preconditions:
-;Registers changed:
+;This procedure writes out the title of the program
+;and explains what it does.
+;Receives: titleMess, prompt01, prompt02, prompt03
+;Returns: n/a
+;Preconditions: 4 messages must be passed in
+;Registers changed: edx
 ;---------------------------------------
 introduction PROC
 
 	push	ebp
 	mov		ebp, esp
-	mov		edx, [ebp+8]
+	mov		edx, [ebp+8]	;titleMess
 	call	WriteString
-	mov		edx, [ebp+20]
+	mov		edx, [ebp+20]	;prompt01
 	call	WriteString
-	mov		edx, [ebp+16]
+	mov		edx, [ebp+16]	;prompt02
 	call	WriteString
-	mov		edx, [ebp+12]
+	mov		edx, [ebp+12]	;prompt03
 	call	WriteString
 	pop		ebp
 	ret		16		;offset of a word is 4 bits * 4 = 12
@@ -111,11 +130,13 @@ introduction PROC
 introduction ENDP
 
 ;----------------------------------------
-;
-;Receives:
-;Returns:
-;Preconditions:
-;Registers changed:
+;This procedure prompts the user to enter in
+;the number of random numbers to be generated
+;between 100 and 999.
+;Receives: userNum, prompt01, errorMess
+;Returns: userNum in eax
+;Preconditions: variable and 2 messages must be passed in
+;Registers changed: edx, eax
 ;---------------------------------------
 getData	PROC
 
@@ -144,11 +165,12 @@ getData	PROC
 getData ENDP
 
 ;----------------------------------------
-;
-;Receives:
-;Returns:
-;Preconditions:
-;Registers changed:
+;This procedure fills the array passed in
+;with random numbers between 100 and 999
+;Receives: userNum, numArray
+;Returns: numArray sorted
+;Preconditions: must have an array with values in it and the length of that array. HI and LO also need to be set and Randomize must have been called
+;Registers changed: ecx, edi, eax
 ;---------------------------------------
 fillArray	PROC
 
@@ -157,7 +179,7 @@ fillArray	PROC
 	mov		ecx, [ebp+8]	;userNum
 	mov		edi, [ebp+12]	;numArray
 
-	generator:				;fills array with nubmers between 100 and 999
+	generator:				;fills array with numbers between 100 and 999
 		mov		eax, HI
 		sub		eax, LO
 		inc		eax
@@ -173,14 +195,14 @@ fillArray	PROC
 fillArray	ENDP
 
 ;----------------------------------------
-;
-;Receives:
-;Returns:
-;Preconditions:
-;Registers changed:
+;This procedure displays the contents of the array passed in.
+;Receives:numArray, userNum, spaces
+;Returns: writes the contents of the array in rows of 10 integers
+;Preconditions: must have an array with values in it and the length of that array as well as spaces to put between them
+;Registers changed: esi, ecx, ebx, eax
 ;---------------------------------------
 
-displayArray	PROC ;This coded is modified from the poperpoint slide "Display: version 0.1 (register indirect) in lecture 20
+displayArray	PROC ;This coded is modified from the powerpoint slide "Display: version 0.1 (register indirect) in lecture 20
 
 	push	ebp
 	mov		ebp, esp
@@ -207,6 +229,14 @@ displayArray	PROC ;This coded is modified from the poperpoint slide "Display: ve
 	ret		16
 
 displayArray	ENDP
+
+;----------------------------------------
+;This procedure uses bubblesort to sort the list from greatest to least
+;Receives:numArray, userNum
+;Returns: sorted array
+;Preconditions: must have an array with values in it and the length of that array
+;Registers changed: ecx, esi, eax
+;---------------------------------------
 
 sortList		PROC		;Bubblesort code taken from textbook page 375
 
@@ -235,5 +265,48 @@ sortList		PROC		;Bubblesort code taken from textbook page 375
 		ret		8
 
 sortList		ENDP
+
+;----------------------------------------
+;This procedure displays the median of an array of integers
+;Receives:numArray, userNum
+;Returns: median of array to the screen
+;Preconditions: must have an array with values in it and the length of that array
+;Registers changed: esi, eax, ebx, ecx
+;---------------------------------------
+
+displayMedian PROC
+
+	push	ebp
+	mov		ebp, esp
+	mov		esi, [ebp+12]		;numArray
+	mov		eax, [ebp+8]		;userNum
+	cdq
+	mov		ebx, 2
+	div		ebx
+	cmp		edx, 0				;no remainder = even
+	je		evenNum
+
+	oddNum:
+		mov		ebx, [esi+eax*4]
+		mov		eax, ebx
+		call	WriteDec
+		jmp		finished
+
+	evenNum:
+		dec		eax
+		mov		ebx, [esi+eax*4]
+		inc		eax
+		mov		ecx, [esi+eax*4]
+		mov		eax, ebx
+		add		eax, ecx
+		mov		ebx, 2
+		div		ebx
+		call	WriteDec
+
+	finished:
+		pop		ebp
+		ret		12
+
+displayMedian ENDP
 
 END main
